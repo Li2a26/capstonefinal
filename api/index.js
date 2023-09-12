cfdd3fd24aaeb0cc1d99dac3d14b03e3197f51a2
Lisa Mbuwa <mbuwalisa@gmail.com>
2023-09-12 11:44:18 +0200

    backend push

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const Router =  require("./routes/routes");
+const Router = require("./routes/routes");
 const express = require("express");
 const app = express();
 
-
 app.use(
-    express.json(),
-    cors(),
-    bodyParser.json(),
-    bodyParser.urlencoded({ extended: true }),
-    Router
-)
+  express.json(),
+  cors({ origin: '*' }), // Allow requests from any origin
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: true }),
+  Router
+);
 
 app.listen(5000, () => console.log('Server running at http://localhost:5000'));
